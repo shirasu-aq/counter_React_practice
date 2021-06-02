cf2c709bf72ac32a9be4e5f87d73d97666e8c0da
Risako Shirasu <shirasu@S734.local>
2021-06-02 12:25:29 +0900

    numberをimport

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 // import logo from "./logo.svg";
 import "./App.css";
+import Number from "./Number";
 
 function App() {
   return (
     <main>
       <div className="countainer">
         <h1>Counter</h1>
+        <Number />
       </div>
     </main>
   );
---
+++ src/Number.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./App.css";
+
+function Number() {
+  return <span idName="zerocolor">0</span>;
+}
+
+export default Number;
