a0d94c04b07191d4d97ad9d81dee8ad0fe0a3964
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-06-02 23:24:28 +0900

    クリックイベント設定

--- src/Buttons.js
+++ src/Buttons.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "./App.css";
 
+const Increase = () => {
+  console.log("increase");
+};
+
 function Buttons() {
   return (
     <div className="buttons_container">
       <button className="btn decrease">DECREASE</button>
       <button className="btn">RESET</button>
-      <button className="btn increase">INCREASE</button>
+      <button className="btn increase" onClick={Increase}>
+        INCREASE
+      </button>
     </div>
   );
 }
